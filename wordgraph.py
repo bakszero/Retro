011bc3f9e6d8ecf087477724a780bd540ca8f317
Bakhtiyar <baksoverflow@gmail.com>
2017-06-16 15:24:12 +0530

    Made basic edge graphs

--- wordgraph.py
+++ wordgraph.py
@@ -8,10 +8,16 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
-g = Graph()
 
 
 stop_words = (set(stopwords.words('english')))
+
+#Union operation on sets
+stop_words = stop_words | {",",".",";","(",")","'","?"}
+
+#print (stop_words)
+
+
 sentence_array = []
 preword_array = []
 
@@ -49,7 +55,7 @@ filtered_sent=[]
 xsent = []
 for line in newsentence_array:
     word_tokens =  word_tokenize(line)
-    xsent = [w for w in word_tokens if not w in stop_words]
+    xsent = [w for w in word_tokens if not w in stop_words ]
     filtered_sent.append(xsent)
 
 #print (filtered_sent)
@@ -61,13 +67,37 @@ temp_sent=[]
 for line in filtered_sent:
     for word in line:
         temp_sent.append(wordnet_lemmatizer.lemmatize(word))
-    lemm_sent.append(temp_sent)
+    lemm_sent.append(tuple(temp_sent))
     temp_sent=[]
 
-print (lemm_sent)
+#print (lemm_sent)
 
+flat_list = [item for sublist in lemm_sent for item in sublist]
+#print (flat_list)
 #Create the sentence graph
-sentence_graph = Graph()
+word_graph = Graph()
 #Find the no. of matchings and add to graph as weight
-for line in lemm_sent:
-    for word in 
+
+#set sliding window size
+#sliding_size = 2
+print ((flat_list))
+#Iterate and form the word graph, increase weight when more than 1 word encountered
+for i in range(0,len(flat_list)-1):
+    if flat_list[i] in word_graph.vertList:
+        if flat_list[i+1] in word_graph.vertList[flat_list[i]].connectedto:
+            word_graph.vertList[flat_list[i]].connectedto[flat_list[i+1]] = int(word_graph.vertList[flat_list[i]].connectedto[flat_list[i+1]])+ 1
+        else:
+            word_graph.addEdge(flat_list[i], flat_list[i+1], 1)
+
+            
+    else:
+        word_graph.addEdge(flat_list[i], flat_list[i+1], 1)
+
+
+print (word_graph.vertList['stupid'].connectedto)
+print (word_graph.vertList)
+for i in word_graph.vertList.values():
+    for j, val in i.connectedto.items():
+        print ("{0} --> {1} --> {2}".format(i.getID(), j.getID(), val))
+
+
